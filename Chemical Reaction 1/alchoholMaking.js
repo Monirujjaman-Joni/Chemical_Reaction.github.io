901758a87b01b3a0591617d8682568f23bf83843
Monirujjaman-Joni <124278532+Monirujjaman-Joni@users.noreply.github.com>
2023-07-01 03:35:05 +0600

    Add files via upload

---
+++ Chemical Reaction 1/alchoholMaking.js
@@ -0,0 +1,4 @@
+function alchoholMaking(a) {
+    var alchoholMaking_step = `C<sub>${a}</sub>H<sub>${2 * a + 1}</sub>Cl + NaOH(aq) &#10230; NaCl + C<sub>${a}</sub>H<sub>${2 * a + 1}</sub>OH <br>`;
+    return alchoholMaking_step;
+}
---
+++ Chemical Reaction 1/main.js
@@ -0,0 +1,55 @@
+var x = document.querySelector("#div");
+function main(p, q) {
+    var n = p;
+    var m = q;
+    if ((n > m || n < m) && n > 0 && m > 0 && n!=m) {
+        var arr = [];
+        console.log(n);
+        console.log(m);
+        function operate(a) {
+            var y;
+            if (a == n) {
+    
+                return m;
+            }
+    
+    
+            else if (a % 2 != 0 && a != n) {
+                y = a + 1;
+                console.log(y);
+                arrayInput(`${decarbo(y)}<br>`);
+                return operate(y);
+    
+            }
+            else if (a % 2 == 0 && a != n && a > n) {
+                y = a / 2;
+                console.log(y);
+                arrayInput(wurtzReaction(y) + "<br>");
+                return operate(y);
+            }
+            else if (a % 2 == 0 && a != n && a < n) {
+                y = a + 1;
+                console.log(y);
+                arrayInput(decarbo(y) + "<br>");
+                return operate(y);
+            }
+            return arr;
+
+        
+        }
+    
+        console.log(operate(m));
+        function arrayInput(i) {
+            return arr.push(i);
+        }
+          
+        console.log(arr.reverse());
+        for (var j = 0; j < arr.length; j++) {
+            x.innerHTML += arr[j] + "<br>";
+        }
+    }
+    else {
+        x.innerHTML = "Invalid input";
+    }
+    
+}
